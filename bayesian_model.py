9ff26dbf0ae4548b7f386186ab97772fff36cd51
devtkrenowiczoverholtzer <devtkrenowiczoverholtzer@gmail.com>
2024-09-19 15:51:47 -0700

    Imports

---
+++ bayesian_model.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib as plt 
+from sklearn.naive_bayes import GaussianNB
+# guassian naive bayes classifier GaussianNB
+from sklearn.model_selection import train_test_split
+# test training split function
+import seaborn as sns 
+# seaborn to look at coorelation plot 
+
+
